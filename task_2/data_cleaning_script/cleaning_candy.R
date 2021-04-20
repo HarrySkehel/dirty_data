c2048e0f77dc894a901425c83083e060f8a9d1e7
HarrySkehel <Harry.skehel@gmail.com>
2021-04-20 12:05:29 +0100

    candy_2015 semi cleaned

--- task_2/data_cleaning_script/cleaning_candy.R
+++ task_2/data_cleaning_script/cleaning_candy.R
@@ -1,11 +1,21 @@
 library(readxl)
 library(tidyverse)
+library(stringr)
 
 candy_2015 <- read_xlsx("raw_data/candy_ranking_data/boing-boing-candy-2015.xlsx")
 candy_2016 <- read_xlsx("raw_data/candy_ranking_data/boing-boing-candy-2016.xlsx")
 candy_2017 <- read_xlsx("raw_data/candy_ranking_data/boing-boing-candy-2017.xlsx")
 
-candy_2015 %>% 
-  pivot_longer()
+colnames(candy_2015)[1] = "[timestamp]"
+colnames(candy_2015)[2] = "[age]"
+colnames(candy_2015)[3] = "[going_out]"
+
+cleanish_2015 <- candy_2015[, grep("^(\\[)", names(candy_2015))]
+  
+  
 
 
+
+
+
+  
